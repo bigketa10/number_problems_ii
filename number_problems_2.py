3f1f18b79490a1e9559b700ed05af3af95280814
Bishal Shrestha <136599301+bigketa10@users.noreply.github.com>
2023-11-17 09:16:41 +0000

    Update number_problems_2.py

--- number_problems_2.py
+++ number_problems_2.py
@@ -2,15 +2,15 @@ import math
 
 
 def roman_numeral(n):
-	symbol = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
-	value = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
-	out = []
-	total = n
-	for i in range (len(symbol)-1,-1,-1):
-		if (total - value[i]) > 0:
-			total = total - value[i]
-			out.append (symbol[i]) 
-	return ''.join(out)
+        symbol = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+        value = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+        out = []
+        total = n
+        for i in range (len(symbol)-1,-1,-1):
+                if (total - value[i]) >= 0:
+                        total = total - value[i]
+                        out.append (symbol[i]) 
+        return ''.join(out)
 
 def total_roman(n):
 	pass
